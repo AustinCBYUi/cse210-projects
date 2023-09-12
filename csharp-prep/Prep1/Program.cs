a1bff325d1ffcb74c7fa0a0f92a21f4ae5bcf05e
AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2023-09-11 19:04:59 -0700

    Removed semicolon for newline.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         //First Name with input.
-        Console.WriteLine("What is your first name?: ");
+        Console.WriteLine("What is your first name? ");
         string first = Console.ReadLine();
 
         //Last name with input
-        Console.WriteLine("What is your last name?: ");
+        Console.WriteLine("What is your last name? ");
         string last = Console.ReadLine();
 
         //f-string to put them together.
