e57a3395065dc51e519ddf575129e48a8a025932
AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2023-09-11 19:03:47 -0700

    Finished assignment 1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //First Name with input.
+        Console.WriteLine("What is your first name?: ");
+        string first = Console.ReadLine();
+
+        //Last name with input
+        Console.WriteLine("What is your last name?: ");
+        string last = Console.ReadLine();
+
+        //f-string to put them together.
+        Console.WriteLine($"Your name is {last}, {first}.");
+        //Testing, useless syntax
+        // Console.WriteLine("Your name is " + last + ", " + first + ".");
     }
 }
