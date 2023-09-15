987a1b9226f925172dbf1c48f90be0aec098ef89
AustinCBYUi <131830841+AustinCBYUi@users.noreply.github.com>
2023-09-14 17:24:43 -0700

    Practicing Loops.

---
+++ sandbox/Sandbox/loops.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program2
+{
+    static void Main(string[] args)
+    {
+        /*
+        Different types of loops,
+while loop
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            Console.WriteLine("Do you want to continue? ");
+            response = Console.ReadLine();
+        }
+
+do-while loop
+        string response;
+
+        do
+        {
+            Console.WriteLine("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
+
+for loop, this loop counts 0 - 9. The i++ is just incrementing of 1
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+for loop that counts 2 to 20 incrementing by 2
+        for (int i = 2; i<= 20; i = i + 2)
+        {
+            Console.WriteLine(i);
+        }
+    
+foreach loop. Must have the type defined, just like declaring any other variable.
+        foreach (string color in colors)
+        {
+            Console.WriteLine(colors);
+        }
+
+Random numbers / choice
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        Console.WriteLine(number)
+        */
+    }
+}
