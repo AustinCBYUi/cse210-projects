61b18eab237eb417cd9195d498d4f57f47cc1e13
AustinCBYUi <131830841+AustinCBYUi@users.noreply.github.com>
2023-10-02 22:36:59 -0700

    Inheritance, definitely an awesome concept!

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Assignment newMainAssignment = new Assignment("Austin Campbell", "Math");
+        MathAssignment newMathAssignment = new MathAssignment("Austin Campbell", "Math", "10-2", "10-40");
+        string math = newMathAssignment.GetSummary() + newMathAssignment.GetHomeWorkProblems();
+
+        Console.WriteLine(math);
+
+        WritingAssignment newWritingAssignment = new WritingAssignment("Austin C", "Geomagnetic Storms",
+        "The Effects of Geomagnetic Storms on Earth.");
+        string writing = newWritingAssignment.GetSummary() + newWritingAssignment.GetWritingInformation();
+        Console.WriteLine(writing);
+
     }
 }
